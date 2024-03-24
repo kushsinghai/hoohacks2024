c4a1fdfbd0b3857f997b12f63c6a79e32bf26411
WaddillP <wadplattuva@gmail.com>
2024-03-24 09:07:13 -0400

    changed so that lyrics is actually gotten where they are (on the first line) rather than after the wpm

--- ML.py
+++ ML.py
@@ -6,6 +6,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import accuracy_score, classification_report
 import scipy.sparse as sp
+from sklearn.impute import SimpleImputer
 
 full_df = pd.DataFrame()
 
@@ -23,6 +24,7 @@ def create_df():
                     with open(file_path, 'r', encoding='utf-8') as file:
                         lines = file.readlines()
                         if len(lines) > 1:
+                            lyrics = lines[0].strip()
                             wpm_line = lines[1].strip()  # Extract the WPM from the second line
                             # Ensure that the second line is indeed a float
                             try:
@@ -31,7 +33,7 @@ def create_df():
                                 # Handle the case where conversion to float fails
                                 print(f"Could not convert WPM value to float in file: {filename}")
                                 wpm = None
-                            lyrics = ''.join(lines[2:])  # Join the rest of the lines to form the lyrics
+                            #lyrics = ''.join(lines[2:])  # Join the rest of the lines to form the lyrics
                             data.append({'lyrics': lyrics, 'artist': artist_name, 'wpm': wpm})
                         else:
                             print(f"File {filename} does not contain enough lines for WPM information.")
@@ -45,7 +47,7 @@ def split_test_train():
     print(full_df)
     X = full_df[['lyrics', 'wpm']]
     y = full_df['artist'] 
-
+    
     # split data into train and test
     # test_size is the proportion of the dataset to include in test split
     # random_state is a seed value for reproducibility of the split
@@ -97,7 +99,11 @@ def main():
     # Stack the WPM feature onto the TF-IDF feature matrix
     X_train_tfidf = sp.hstack((X_train_tfidf, np.array(X_train['wpm']).reshape(-1, 1)), format='csr')
     X_test_tfidf = sp.hstack((X_test_tfidf, np.array(X_test['wpm']).reshape(-1, 1)), format='csr')
+    print(X_test_tfidf)
+    #imputer = SimpleImputer(strategy='mean')
 
+    #X_train_tfidf = imputer.fit_transform(X_train_tfidf)
+    #X_test_tfidf = imputer.transform(X_test_tfidf)
     model = train_naive_bayes(X_train_tfidf, y_train)
     evaluate_model(model, X_test_tfidf, y_test)
     joblib.dump(model, 'naive_bayes_model.pkl')
