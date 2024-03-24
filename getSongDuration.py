9ef0d8bed5db6698f6ebe52974b7425c0a993cd9
WaddillP <wadplattuva@gmail.com>
2024-03-23 21:07:32 -0400

    Wrote getSongDuration.py

---
+++ getSongDuration.py
@@ -0,0 +1,20 @@
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+# Authenticate with the Spotify API
+client_credentials_manager = SpotifyClientCredentials(client_id='69e14015c025495cb095a26340a954f5', client_secret='70f478fc63a545dfb1bb3dd66acb377f')
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+
+def getDuration(artist, songName):
+    # search for the song
+    query = 'track:{} artist:{}'.format(songName, artist)
+    results = sp.search(q=query, type='track')
+    track = results['tracks']['items'][0]
+
+    # get duration of the track
+    duration_ms = track['duration_ms']
+    #print(duration_ms)
+    return duration_ms
+
+
